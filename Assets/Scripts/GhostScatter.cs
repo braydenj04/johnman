0368e92605bcd881423905bbd9f25ff92c03d100
braydenj04 <braydenjeffrey2006@gmail.com>
2025-11-10 21:54:23 +0000

    node direction

--- Assets/Scripts/GhostScatter.cs
+++ Assets/Scripts/GhostScatter.cs
@@ -6,14 +6,21 @@ public class GhostScatter : GhostBehavior
     {
         Node node = other.GetComponent<Node>();
 
-        if (node != null && !this.enabled && !this.ghost.frightened.enabled) 
+        if (node != null && this.enabled && !this.ghost.frightened.enabled) 
         {
             int index = Random.Range(0, node.availableDirections.Count);
 
-            if (node.availableDirections[index] == -this.ghost.movement.direction) 
-            { 
+            if (node.availableDirections[index] == -this.ghost.movement.direction && node.availableDirections.Count > 1) 
+            {
+                index++;
 
+                if(index < node.availableDirections.Count)
+                {
+                    index = 0;
+                }
             }
+
+            this.ghost.movement.SetDirection(node.availableDirections[index]);
         }
     }
 }
