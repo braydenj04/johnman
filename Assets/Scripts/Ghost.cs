6d2f9f1c2ced1e0f9965e952a06092c274999048
braydenj04 <braydenjeffrey2006@gmail.com>
2025-11-10 21:35:10 +0000

    ghost direction

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -48,7 +48,7 @@ public class Ghost : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision) 
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Pacman "))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("pacmanChar"))
         {
             Object.FindAnyObjectByType<GameManager>().PacmanEaten();
         }
--- Assets/Scripts/GhostScatter.cs
+++ Assets/Scripts/GhostScatter.cs
@@ -2,4 +2,13 @@ using UnityEngine;
 
 public class GhostScatter : GhostBehavior
 {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Node node = other.GetComponent<Node>();
+
+        if (node != null && !this.enabled && !this.ghost.frightened.enabled) 
+        {
+
+        }
+    }
 }
