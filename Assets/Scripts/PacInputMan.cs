c7b950684e473d7d208c8d3ea02b6d1d79eca6c4
JarJar247 <johnrob020407@gmail.com>
2025-11-10 18:24:04 +0000

    Pacman input script

--- Assets/Scripts/PacInputMan.cs
+++ Assets/Scripts/PacInputMan.cs
@@ -1,16 +1,33 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Pacman1 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Movement movement {  get; private set; }
+
+    private void Awake()
     {
-        
+        this.movement = GetComponent<Movement>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.movement.SetDirection(Vector2.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.movement.SetDirection(Vector2.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.movement.SetDirection(Vector2.left);
+        }
+        float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
+        this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
